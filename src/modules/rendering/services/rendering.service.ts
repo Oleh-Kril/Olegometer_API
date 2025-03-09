efd9f4e032f71769dc4166e37448da3cd6359941
Oleh-Kril <oleh.kril.t@gmail.com>
2025-03-09 12:34:48 +0200

    improve rendering service

--- src/modules/rendering/services/rendering.service.ts
+++ src/modules/rendering/services/rendering.service.ts
@@ -1,7 +1,7 @@
 import {Injectable, OnModuleDestroy} from '@nestjs/common'
 import {InjectMapper} from "@automapper/nestjs"
 import {Mapper} from "@automapper/core"
-import {Browser, chromium} from 'playwright'
+import {Browser, chromium, Page} from 'playwright'
 
 @Injectable()
 export class RenderingService implements OnModuleDestroy{
@@ -13,7 +13,7 @@ export class RenderingService implements OnModuleDestroy{
 
     async onModuleInit() {
         try {
-            this.browser = await chromium.launch();
+            this.browser = await chromium.launch({headless: false});
         } catch (error) {
             console.error('Error launching browser:', error);
         }
@@ -42,12 +42,37 @@ export class RenderingService implements OnModuleDestroy{
         await page.waitForLoadState('networkidle')
 
         await page.setViewportSize({ width, height: 1080 })
+
+        await this.autoScroll(page)
+
+        await page.evaluate(() => {
+            window.scrollTo(0, 0);
+        });
+
         if (delay) {
             await page.waitForTimeout(delay)
         }
+
         const imageBuffer = await page.screenshot({ type: 'jpeg', fullPage: true })
         await page.close()
 
         return imageBuffer
+    }
+
+    private async autoScroll(page: Page) {
+        await page.evaluate(async () => {
+            await new Promise<void>((resolve) => {
+                let totalHeight = 0;
+                const distance = 100;
+                const timer = setInterval(() => {
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
+                    if (totalHeight >= document.body.scrollHeight) {
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 100);
+            });
+        });
     }
 }
