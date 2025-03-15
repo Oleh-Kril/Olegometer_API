2ef3f7130f30cd6e69b1d23a804fafbd181648bd
Oleh-Kril <oleh.kril.t@gmail.com>
2025-03-15 10:35:13 +0200

    remove code duplicates

--- src/modules/rendering/services/rendering.service.ts
+++ src/modules/rendering/services/rendering.service.ts
@@ -39,7 +39,6 @@ export class RenderingService implements OnModuleDestroy{
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'Accept-Language': 'en-US,en;q=0.9'
         });
-        await page.goto(url, { waitUntil: 'networkidle' })
 
         try {
             await page.goto(url, { waitUntil: 'networkidle', timeout: 30000 });
